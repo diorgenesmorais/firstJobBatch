2eb14926a2acb4d3a09c21e61620dfd49cb0951f
Diorgenes Morais <diorgenesmorais@gmail.com>
2024-12-03 21:57:13 -0300

    create first job batch project

---
+++ src/main/java/com/dms/firstJobBatch/FirstJobBatchApplication.java
@@ -0,0 +1,13 @@
+package com.dms.firstJobBatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstJobBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstJobBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dms/firstJobBatch/FirstJobBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dms.firstJobBatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstJobBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
